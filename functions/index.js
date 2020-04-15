457cde980df807b03198b2ee21aec3383ce19866
Adam Heinrich <adameksopot@gmail.com>
2020-04-15 17:15:35 +0200

    index opisy

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,35 @@
 
 
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+const admin = require('firebase-admin');// importy zaleznosci
+const functions = require('firebase-functions');// importy zaleznosci 
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp(functions.config().firebase); //logowanie jako admin
 
-let db = admin.firestore();
+let db = admin.firestore(); // referencja do bazy danych aby nie pisac admin firestory
 exports.getExcel = functions.https.onRequest(async (req, res) => {
-   
+
     try {
 
         let docRef = db.collection('answers')
         res.send()
-        
+
     }
     catch (e) {
         console.log(e)
-    }})
+    }
+})
+// exports to eksportowanie do firebase funkcji (upublicznianie)
 exports.submitAns = functions.https.onRequest(async (req, res) => {
-    const timestamp = Date.now().toString()
-    console.log(req.body["0"])
-    try {
-        let docRef = db.collection('answers').doc(timestamp);
-        let setDoc = docRef.set(req.body);
-    }
-    catch (e) {
-        console.log(e)
-    }
-    res.send("Dziekuje za udzial w ankiecie!")
+    const timestamp = Date.now().toString() //unikalne id dokumentu
+
+    let docRef = db
+        .collection('answers')
+        .doc(timestamp) //twprze nowy dokument
+        .set(req.body) // odpowiedzi 
+        .catch(console.warn);
+
+    res.send("Dziekuje za udzial w ankiecie!") // strona.html
 });
+
+
+// 
